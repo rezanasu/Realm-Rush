26b6090b1db1c3edcc194779b46450e25059dc16
Reza Nasution <rezanasution82@gmail.com>
2019-11-25 00:12:05 +0100

    Basic Pathfinding

--- Assets/Scripts/CubeEditor.cs
+++ Assets/Scripts/CubeEditor.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
 {
     [Range(1f,20f)][SerializeField] float gridSize = 10f;
+
+    TextMesh textMesh;
 
     // Update is called once per frame
     void Update()
@@ -15,5 +18,10 @@ public class EditorSnap : MonoBehaviour
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
 
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        string textLabel = snapPos.x / gridSize + "." + snapPos.z / gridSize;
+        textMesh.text = textLabel;
+        gameObject.name = textLabel;
     }
 }
