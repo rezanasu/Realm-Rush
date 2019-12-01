0e319c8a2192c24751c1fee1810171f05836fe8e
Reza Nasution <rezanasution82@gmail.com>
2019-12-01 05:23:42 +0100

    Tower Shooting Mechanism

---
+++ Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    [SerializeField] int hitpoints = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        if(hitpoints <= 0)
+        {
+            KillEnemy();
+        }
+    }
+
+    private void KillEnemy()
+    {
+        Destroy(gameObject);
+    }
+
+    void ProcessHit()
+    {
+        hitpoints--;
+        print("current hitpoints are: " + hitpoints);
+    }
+}
